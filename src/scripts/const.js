68b971d08f94b293d5f8a9748c885b242f7900be
Dennis Kugelmann <kugelmann.dennis@gmail.com>
2016-08-21 23:54:18 +0200

    Login confirmation page

---
+++ src/scripts/const.js
@@ -0,0 +1,9 @@
+var HTML_PROTOCOL = 'http://';
+var WS_PROTOCOL = 'ws://';
+
+var API_BASE_URL = 'api.deepspace.onl:5555';
+
+var API_LOGIN_URL = WS_PROTOCOL + API_BASE_URL + '/login';
+
+var API_CONFIRM_URL = HTML_PROTOCOL + API_BASE_URL + '/confirm';
+var CONFIRM_TIMEOUT = 2000;
