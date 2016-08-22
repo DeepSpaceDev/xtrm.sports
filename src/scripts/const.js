bd09a9e0b04995f70b83f24f1acd99424111e656
Dennis Kugelmann <kugelmann.dennis@gmail.com>
2016-08-23 00:53:29 +0200

    Changes on background image
    Changed how background images are handled and appear visible on screen. Background images have no longer blur, they have got a semi-transparent black overlay.

--- src/scripts/const.js
+++ src/scripts/const.js
@@ -1,11 +1,13 @@
 var HTML_PROTOCOL = 'http://';
 var WS_PROTOCOL = 'ws://';
 
-var API_BASE_URL = 'api.deepspace.onl:5555';
+var API_BASE_URL = 'api.deepspace.onl:5555/';
 
-var API_LOGIN_URL = WS_PROTOCOL + API_BASE_URL + '/login';
+var API_LOGIN_URL = WS_PROTOCOL + API_BASE_URL + 'login/';
 
-var API_CONFIRM_URL = HTML_PROTOCOL + API_BASE_URL + '/confirm';
+var API_CONFIRM_URL = HTML_PROTOCOL + API_BASE_URL + 'confirm/';
 var CONFIRM_TIMEOUT = 2000;
 
-var API_REGISTER_URL = HTML_PROTOCOL + API_BASE_URL + '/register';
+var API_REGISTER_URL = HTML_PROTOCOL + API_BASE_URL + 'register/';
+
+var BASE_URL = HTML_PROTOCOL + 'v2.xtrm-sports.com/';
